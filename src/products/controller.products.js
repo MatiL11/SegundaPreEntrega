7af61c6551b51e63e44a34a1d799059d5e4f5f1d
Matias <matiaslisandro9@gmail.com>
2023-04-18 17:45:32 -0300

    actualizado

--- src/products/controller.products.js
+++ src/products/controller.products.js
@@ -5,8 +5,55 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   try {
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const sort = req.query.sort || null;
+
     const products = await Products.find();
-    res.json({ status: "success", message: products });
+
+    if (!products) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "No products found" });
+    }
+
+    let filteredProducts = products;
+    //console.log(req.query);
+    //console.log(filteredProducts);
+    if (sort) {
+      filteredProducts = filteredProducts.sort((a, b) => {
+        if (sort === "asc") {
+          return a.price - b.price;
+        } else if (sort === "desc") {
+          return b.price - a.price;
+        }
+      });
+    }
+
+    const totalPages = Math.ceil(filteredProducts.length / limit);
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const paginatedProducts = filteredProducts.slice(startIndex, endIndex);
+    //console.log(paginatedProducts);
+
+    const response = {
+      payload: paginatedProducts,
+      status: "success",
+      totalPages: totalPages,
+      prevPage: page > 1 ? page - 1 : null,
+      nextPage: page < totalPages ? page + 1 : null,
+      page: page,
+      hasPrevPage: page > 1,
+      hasNextPage: page < totalPages,
+      prevLink:
+        page > 1 ? `/?limit=${limit}&page=${page - 1}&sort=${sort}` : null,
+      nextLink:
+        page < totalPages
+          ? `/?limit=${limit}&page=${page + 1}&sort=${sort}`
+          : null,
+    };
+
+    res.send(response);
   } catch (error) {
     res.status(400).json({ status: "error", error });
   }
